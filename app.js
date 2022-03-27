66b21ebc9f8e9b888a5f26d5052ad280378faa4d
Chandu <68140710+chandusurisetty@users.noreply.github.com>
2022-03-27 18:44:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+const btn = document.querySelector("button");
+const roll = document.querySelector("#dice")
+const faces = document.querySelector("#faces")
+const divbar = document.querySelector(".dicebar")
+const h = document.querySelector(".h");
+const div = document.createElement("div")
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    divbar.innerHTML = ""
+
+    const barfar = document.querySelectorAll(".diceinfo")
+    const h2bars = document.querySelectorAll(".hai")
+    console.log(h2bars)
+
+    for (let i = 0; i < barfar.length; i++) {
+        barfar[i].remove()
+    }
+    for (let i = 0; i < h2bars.length; i++) {
+        h2bars[i].remove()
+    }
+
+
+    let count = 0;
+    const totaldice = parseInt(roll.value);
+    const totalfaces = parseInt(faces.value);
+    for (i = 1; i <= totaldice; i++) {
+        const div = document.createElement("div")
+        const p = document.createElement("p")
+
+        const rand = Math.floor(Math.random() * totalfaces) + 1;
+        count += rand;
+
+
+
+
+
+        div.innerHTML = `<div class="diceinfo"><p><p>dice ${i}  <br> value ${rand}</p></p></div>`
+
+        div.appendChild(p)
+        divbar.appendChild(div)
+
+
+
+    }
+    const h2 = document.createElement("h2");
+    h2.classList.add("hai")
+    h2.innerHTML = `Total number of faces on dice <span class="a">${totalfaces}</span><br>
+                    Total number of dices roller <span class="a">${totaldice}</span><br>
+                    Total count of dice is <span class="a">${count}</span>`
+    h.appendChild(h2)
+    roll.value = "1",
+        faces.value = "4"
+
+})
